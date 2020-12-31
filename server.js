38a63d9d5e199006c470b4e8194ccb3ab02c0599
yarin13 <48176515+yarin13@users.noreply.github.com>
2020-12-31 13:20:58 +0200

    last update

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/project'));
+app.use(express.static(__dirname + '/dist/project'));
 
-app.get('/*', (req, res) =>  
-res.sendFile('index.html', {root: 'dist/project/'}),
-);
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/project/index.html'));});
+
+
 
 app.listen(process.env.PORT || 8080);
