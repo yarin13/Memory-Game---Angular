d6cefc80b507e4b9c1bbd0d16fe9951be0369676
yarin13 <48176515+yarin13@users.noreply.github.com>
2020-12-31 04:17:38 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,12 +5,22 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/project'));
+//app.use(express.static(__dirname + '/dist/project'));
 
-app.get('/*', function(req,res) {
+
+app.use(express.static('./dist/project'));
+
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/project/index.html'));
-});
+// res.sendFile(path.join(__dirname+'/dist/project/index.html'));
+// });
+
+
+
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/project/'}
+  );
+  });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
