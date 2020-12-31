c5d5dc2c8e929433ca67290578dc9bc5569e29c5
yarin13 <48176515+yarin13@users.noreply.github.com>
2020-12-31 04:22:49 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 //app.use(express.static(__dirname + '/dist/project'));
 
 
-app.use(express.static('./dist/project'));
+//app.use(express.static('./dist/project'));
 
 // app.get('/*', function(req,res) {
     
