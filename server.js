d4d4774c2f8098349302f9414b7e4e98772c190b
yarin13 <48176515+yarin13@users.noreply.github.com>
2020-12-31 15:43:13 +0200

    last update

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/project'));
+app.use(express.static('./dist/Project'));
 
 app.get('/*', (req,res)=>
-    res.sendFile('index.html',{root:'dist/project/'}),);
+    res.sendFile('index.html',{root:'dist/Project/'}),);
 
 
 
